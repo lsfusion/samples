a81da7ea193219db3a596028e7240dc41cf034c3
AlexKirkouski <alex.kirkouski@gmail.com>
2018-07-09 11:00:30 +0300

    Moved CRM, Features to another repository

---
+++ features/src/main/java/samples/TestCustomActionProperty.java
@@ -0,0 +1,22 @@
+package samples;
+
+import lsfusion.server.classes.ValueClass;
+import lsfusion.server.data.SQLHandledException;
+import lsfusion.server.logics.property.ClassPropertyInterface;
+import lsfusion.server.logics.property.ExecutionContext;
+import lsfusion.server.logics.scripted.ScriptingActionProperty;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+
+import java.sql.SQLException;
+
+public class TestCustomActionProperty extends ScriptingActionProperty {
+
+    public TestCustomActionProperty(ScriptingLogicsModule LM) {
+        super(LM);
+    }
+
+    @Override
+    protected void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+        System.out.println("hi");
+    }
+}
