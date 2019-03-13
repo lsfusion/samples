64ee43e93d0711d8f82122ba6a782a532fef579c
AlexKirkouski <alex.kirkouski@gmail.com>
2019-03-13 14:30:11 +0300

    Refactoring packages in server

--- features/src/main/java/samples/TestCustomActionProperty.java
+++ features/src/main/java/samples/TestCustomActionProperty.java
@@ -1,10 +1,11 @@
 package samples;
 
+import lsfusion.server.classes.ValueClass;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
-import lsfusion.server.language.ScriptingActionProperty;
-import lsfusion.server.language.ScriptingLogicsModule;
+import lsfusion.server.logics.scripted.ScriptingActionProperty;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 
 import java.sql.SQLException;
 
