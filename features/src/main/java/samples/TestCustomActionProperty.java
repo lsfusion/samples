ca946308dd14856821f11653dda75111e6bb6bc2
AlexKirkouski <alex.kirkouski@gmail.com>
2019-03-18 10:11:42 +0300

    Refactored packages in server

--- features/src/main/java/samples/TestCustomActionProperty.java
+++ features/src/main/java/samples/TestCustomActionProperty.java
@@ -1,9 +1,9 @@
 package samples;
 
-import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 import lsfusion.server.language.ScriptingLogicsModule;
-import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
+import lsfusion.server.logics.property.classes.ClassPropertyInterface;
+import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
 public class TestCustomActionProperty extends InternalAction {
 
