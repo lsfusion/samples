7606867a52c8d8fd163dcabc6f3df1e70e53d754
AFilipchyk <filipchyk.andrei@gmail.com>
2022-01-18 10:22:08 +0300

    EXTNULL option to export NULL values in JSON / XML

--- documentation/src/main/main.py
+++ documentation/src/main/main.py
@@ -100,7 +100,7 @@ class LSFLexer(RegexLexer):
                     'DATA', 'DBF', 'DEFAULT', 'DEFAULTCOMPARE', 'DELAY', 'DELETE', 'DESC', 'DESIGN', 'DIALOG', 'DO',
                     'DOC', 'DOCKED', 'DOCX', 'DRAW', 'DRAWROOT', 'DRILLDOWN', 'DROP', 'DROPCHANGED', 'DROPPED', 'ECHO',
                     'EDIT', 'ELSE', 'EMAIL', 'END', 'EQUAL', 'ESCAPE', 'EVAL', 'EVENTID', 'EVENTS', 'EXCEPTLAST',
-                    'EXCLUSIVE', 'EXEC', 'EXPAND', 'EXPORT', 'EXTEND', 'EXTERNAL', 'EXTID', 'EXTKEY', 'FALSE', 'FIELDS',
+                    'EXCLUSIVE', 'EXEC', 'EXPAND', 'EXPORT', 'EXTEND', 'EXTERNAL', 'EXTID', 'EXTKEY', 'EXTNULL', 'FALSE', 'FIELDS',
                     'FILTER', 'FILTERGROUP', 'FILTERGROUPS', 'FILTER', 'FILTERS', 'FINALLY', 'FIRST', 'FIXED', 'FLEX', 'FLOAT',
                     'FOLDER', 'FOOTER', 'FOR', 'FOREGROUND', 'FORM', 'FORMEXTID', 'FORMS', 'FORMULA', 'FROM', 'FULL',
                     'GET', 'GLOBAL', 'GOAFTER', 'GRID', 'GRIDBOX', 'GROUP', 'GROUPCHANGE', 'HALIGN', 'HEADER',
